8bf59ce030da8aff176fdb3c66088d9908e09f5d
k3nn3th255 <123401417+k3nn3th255@users.noreply.github.com>
2024-10-20 11:11:26 -0600

    termine ejercicio 3 de sugunda practica

---
+++ EJERCICIO 3/script.js
@@ -0,0 +1,6 @@
+// Archivo: script.js
+document.getElementById("miBoton").addEventListener("click", function() {
+    // Cambiar el contenido del párrafo usando innerHTML
+    document.getElementById("miParrafo").innerHTML = "¡El contenido del párrafo ha cambiado! Muchas gracias profe por tomarse el tiempo de ver a detalle los trabajos"+
+    "un saludo y espero la calificacion atento jajaja ";
+});
